a16f3f907e1d39739756d659f2dd52f4b9667777
deeqkko <kimmo.koivusalo@gmail.com>
2018-02-15 09:06:01 +0200

    Tyhjä projekti. 1.Commmit

---
+++ src/kuti_server/Kuti_server.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuti_server;
+
+/**
+ *
+ * @author hamkTeam4/deeqkko
+ */
+public class Kuti_server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
